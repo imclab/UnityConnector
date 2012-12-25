fb18a9f64603c3991f301aab4cd2ddbd23bdcc45
sassembla <toru.inoue@kissaki.tv>
2012-12-25 11:04:41 +0900

    update

--- UnityConnector/AppDelegate.m
+++ UnityConnector/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "KSUnityBooter.h"
+#import "KSUnityConnector.h"
 
 #import "SampleTitleViewController.h"
 #import "SamplePinViewControler.h"
@@ -40,7 +40,7 @@ void uncaughtExceptionHandler(NSException *exception) {
     
     
     messenger = [[MessengerSystem alloc] initWithBodyID:self withSelector:@selector(receiver:) withName:CONNECTOR_MASTER];
-    KSUnityBooter * unityBoot = [[KSUnityBooter alloc]initKSUnityBooterWithMasterName:CONNECTOR_MASTER];
+    KSUnityConnector * unityConnect = [[KSUnityConnector alloc]initKSUnityConnectorWithMasterName:CONNECTOR_MASTER];
     
     sampleVCont = [[SampleTitleViewController alloc]initSampleTitleViewControllerWithMasterName:CONNECTOR_MASTER];
     
@@ -61,13 +61,13 @@ void uncaughtExceptionHandler(NSException *exception) {
 		/*
 		 ignite Unity
 		 */
-        [messenger call:KS_UNITYBOOTER withExec:KS_UNITYBOOTER_EXEC_INITIALIZE,
+        [messenger call:KS_UNITYCONNECTOR withExec:KS_UNITYCONNECTOR_EXEC_INITIALIZE,
          [messenger tag:@"application" val:m_application],
          [messenger tag:@"launchOptions" val:m_launchOptions],
          nil];
 	}
 	
-	if ([exec isEqualToString:KS_UNITYBOOTER_EXEC_VIEWCANBEAPPEND]) {
+	if ([exec isEqualToString:KS_UNITYCONNECTOR_EXEC_VIEWCANBEAPPEND]) {
 		/*
 		 generate pinView(back button inside.)
 		 */
@@ -77,7 +77,7 @@ void uncaughtExceptionHandler(NSException *exception) {
 		
 		
 		[baseView.view addSubview:samplePinViewCont.view];
-		[messenger call:KS_UNITYBOOTER withExec:KS_UNITYBOOTER_EXEC_ADD_VIEW,
+		[messenger call:KS_UNITYCONNECTOR withExec:KS_UNITYCONNECTOR_EXEC_ADD_VIEW,
 		 [messenger tag:@"view" val:baseView.view],
 		 nil];
 	}
@@ -85,31 +85,31 @@ void uncaughtExceptionHandler(NSException *exception) {
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    [messenger call:KS_UNITYBOOTER withExec:KS_UNITYBOOTER_EXEC_WILLRESIGNACTIVE,
+    [messenger call:KS_UNITYCONNECTOR withExec:KS_UNITYCONNECTOR_EXEC_WILLRESIGNACTIVE,
      [messenger tag:@"application" val:application],
      nil];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    [messenger call:KS_UNITYBOOTER withExec:KS_UNITYBOOTER_EXEC_DIDENTERBACKGROUND,
+    [messenger call:KS_UNITYCONNECTOR withExec:KS_UNITYCONNECTOR_EXEC_DIDENTERBACKGROUND,
      [messenger tag:@"application" val:application],
      nil];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    [messenger call:KS_UNITYBOOTER withExec:KS_UNITYBOOTER_EXEC_WILLENTERFOREGROUND,
+    [messenger call:KS_UNITYCONNECTOR withExec:KS_UNITYCONNECTOR_EXEC_WILLENTERFOREGROUND,
      [messenger tag:@"application" val:application],
      nil];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    [messenger call:KS_UNITYBOOTER withExec:KS_UNITYBOOTER_EXEC_DIDBECOMEACTIVE,
+    [messenger call:KS_UNITYCONNECTOR withExec:KS_UNITYCONNECTOR_EXEC_DIDBECOMEACTIVE,
      [messenger tag:@"application" val:application],
      nil];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    [messenger call:KS_UNITYBOOTER withExec:KS_UNITYBOOTER_EXEC_WILLTERMINATE,
+    [messenger call:KS_UNITYCONNECTOR withExec:KS_UNITYCONNECTOR_EXEC_WILLTERMINATE,
      [messenger tag:@"application" val:application],
      nil];
 }
